02409c8bd34f1fb392559fc552ef3ea84aad38f6
applepiren <fiorentinaamaraputri22@gmail.com>
2023-09-15 09:47:21 +0700

    Tugas 2 DDP

---
+++ 02 Masukan dan Keluaran/Fiorentina_soal_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float y1, y2;
+    cin >> y1 >> y2;
+
+    cout << y1 + y2 << endl;
+    cout << y1 - y2 << endl;
+    cout << y1 * y2 << endl;
+    cout << y1 / y2 << endl;
+
+    return 0;
+}
